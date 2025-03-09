90611832fe21365a9874cddd544db37c31edc677
Justin Su <justin.su348@gmail.com>
2025-03-09 19:32:30 -0400

    changed area variable to const

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ function Heron(event) {
     const c = parseFloat(document.getElementById("sideC").value);
 
     if (a < b+c && b < a+c && c < a+b) {
-        let area = 0.25*Math.sqrt(4*a*a*b*b - Math.pow(a*a + b*b - c*c, 2));
+        const area = 0.25*Math.sqrt(4*a*a*b*b - Math.pow(a*a + b*b - c*c, 2));
         document.getElementById("Area").value = `Area: ${area.toFixed(2)} units squared`; 
     }
     else{
