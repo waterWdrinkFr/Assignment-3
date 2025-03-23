f509c1a7a21794d068f90b51ae721eca7b15702b
Justin Su <justin.su348@gmail.com>
2025-03-23 17:46:54 -0400

    optimized newton's method code

--- script.js
+++ script.js
@@ -49,24 +49,24 @@ document.getElementById("ambig").addEventListener('submit', (event) => {
     }
 })
 
-document.getElementById("newton").addEventListener('submit', (event) => {
+document.getElementById("newton").addEventListener("submit", (event) => {
     event.preventDefault();
     let x0 = parseFloat(document.getElementById("guess").value);
-    let x1 = approximateRoot(x0);
 
-    while (Math.abs(x0-x1) > 0.0001) {
-        x0 = x1;
-        x1 = approximateRoot(x0);
+    function newton(x0) {
+        let y = 6 * Math.pow(x0, 4) - 13 * Math.pow(x0, 3) - 18 * Math.pow(x0, 2) + 7 * x0 + 6;
+        let yPrime = 24 * Math.pow(x0, 3) - 39 * Math.pow(x0, 2) - 36 * x0 + 7;
+        let x1 = x0 - y / yPrime;
+
+        if (Math.abs(x0 - x1) <= 0.0001) {
+            return x1;
+        }
+        return newton(x1);
     }
 
-    document.getElementById("root").value = `Approximated Root: ${x1.toFixed(3)}`;
-})
-
-function approximateRoot(x) {
-    let y = 6*Math.pow(x, 4) - 13*Math.pow(x, 3) - 18*Math.pow(x, 2) + 7*x + 6;
-    let yPrime = 24*Math.pow(x, 3) - 39*Math.pow(x, 2) - 36*x + 7;
-    return x - y/yPrime;
-}
+    let root = newton(x0);
+    document.getElementById("root").value = `Approximated Root: ${root.toFixed(3)}`;
+});
 
 document.getElementById("poly").addEventListener('submit', (event) => {
     event.preventDefault();
